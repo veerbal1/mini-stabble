db4cd45ca693f307e9f82ec9c20378d3eca62ff5
veerbal1 <veerbalsingh1@gmail.com>
2025-12-25 10:58:43 +0530

    refactor: Rearrange imports and enhance `calc_invariant` function with initial calculations for stable swap invariant.

--- programs/mini-stabble/src/math/stable.rs
+++ programs/mini-stabble/src/math/stable.rs
@@ -1,4 +1,7 @@
-use bn::{uint192, U192, safe_math::{CheckedMulDiv, Downcast}};
+use bn::{
+    safe_math::{CheckedMulDiv, Downcast},
+    uint192, U192,
+};
 
 pub const AMP_PRECISION: u64 = 1000;
 pub const MIN_AMP: u64 = 1;
@@ -7,6 +10,13 @@ pub const MAX_LOOP_LIMIT: u64 = 256;
 
 /// Calculates the StableSwap invariant D using Newton-Raphson iteration.
 pub fn calc_invariant(amplification: u64, balances: &Vec<u64>) -> Option<u64> {
-    // We'll fill this in together
+    // An^n(x + y) + D - An²D - D³/(4xy) = 0
+    let sum: u64 = balances.iter().sum();
+    let num_of_tokens = balances.len() as u64;
+
+    let amp_times_total = amplification.checked_mul(num_of_tokens)?;
+
+    let sum_u192 = uint192!(sum);
+    let mut invariant = sum_u192;
     todo!()
 }
