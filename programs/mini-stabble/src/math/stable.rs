d13c4cf025a8406e61653a6cb31a580ccc35fd93
veerbal1 <veerbalsingh1@gmail.com>
2025-12-25 10:38:07 +0530

    feat: Add `calc_invariant` function signature and adjust stable swap math constant types.

--- programs/mini-stabble/src/math/stable.rs
+++ programs/mini-stabble/src/math/stable.rs
@@ -1,4 +1,12 @@
-pub const AMP_PRECISION: u128 = 1000;
-pub const MIN_AMP: u128 = 1;
-pub const MAX_AMP: u128 = 10_000;
+use bn::{uint192, U192, safe_math::{CheckedMulDiv, Downcast}};
+
+pub const AMP_PRECISION: u64 = 1000;
+pub const MIN_AMP: u64 = 1;
+pub const MAX_AMP: u64 = 10_000;
 pub const MAX_LOOP_LIMIT: u64 = 256;
+
+/// Calculates the StableSwap invariant D using Newton-Raphson iteration.
+pub fn calc_invariant(amplification: u64, balances: &Vec<u64>) -> Option<u64> {
+    // We'll fill this in together
+    todo!()
+}
