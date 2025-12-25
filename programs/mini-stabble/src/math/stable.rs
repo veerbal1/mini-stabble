1b8e0bc10f4ab75b901c5a1fb087de80288eb34f
veerbal1 <veerbalsingh1@gmail.com>
2025-12-26 00:30:41 +0530

    refactor: Update `calc_invariant` function to handle zero balances and clarify mathematical expressions for stable swap calculations.

--- programs/mini-stabble/src/math/stable.rs
+++ programs/mini-stabble/src/math/stable.rs
@@ -1,3 +1,4 @@
+use anchor_lang::require;
 use bn::{
     safe_math::{CheckedMulDiv, Downcast},
     uint192, U192,
@@ -10,13 +11,20 @@ pub const MAX_LOOP_LIMIT: u64 = 256;
 
 /// Calculates the StableSwap invariant D using Newton-Raphson iteration.
 pub fn calc_invariant(amplification: u64, balances: &Vec<u64>) -> Option<u64> {
-    // An^n(x + y) + D - An²D - D³/(4xy) = 0
+    // An^n × S + D = An^n × D + D^(n+1) / (n^n × P)
     let sum: u64 = balances.iter().sum();
-    let num_of_tokens = balances.len() as u64;
+    if sum == 0 {
+        return Some(0);
+    }
 
-    let amp_times_total = amplification.checked_mul(num_of_tokens)?;
-
+    let num_tokens = balances.len() as u64;
+    let ann = amplification.checked_mul(num_tokens)?;
     let sum_u192 = uint192!(sum);
-    let mut invariant = sum_u192;
-    todo!()
+
+    let initial = sum_u192;
+
+    // D_new = (Ann × S + n × D_P) × D / ((Ann - 1) × D + (n + 1) × D_P)
+
+
+    Some(0)
 }
